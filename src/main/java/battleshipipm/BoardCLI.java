c8e37d8f9d8052c1395dd67a8ed9d6c105107ca4
shibani <shibani@8thlight.com>
2018-03-14 14:33:18 -0400

    Refactoring boardCli

--- src/main/java/battleshipipm/BoardCLI.java
+++ src/main/java/battleshipipm/BoardCLI.java
@@ -5,69 +5,74 @@ public class BoardCLI {
 
     private Board board;
     private String[] alpha = new String[]{ "A", "B", "C", "D", "E", "F", "G", "H", "I", "J" };
-    private String colHeaders =  "      0     1     2     3     4     5     6     7     8     9    \n";
-    private String horizontalDivider = "   +=====+=====+=====+=====+=====+=====+=====+=====+=====+=====+ \n";
+    private String colHeaders =  "\n      0     1     2     3     4     5     6     7     8     9    ";
+    private String horizontalDivider = "\n   +=====+=====+=====+=====+=====+=====+=====+=====+=====+=====+ ";
     private String firstAlpha = " A | ";
     private String verticalDivider = " | ";
     private String singleSpace = " ";
-    private String newLine = "\n";
 
     BoardCLI(Board board){
         this.board = board;
     }
 
     public void print(Board board){
+        this.printHeader();
+
         ArrayList<String> positions = board.getPositions();
-
-        System.out.print(colHeaders);
-        System.out.println(horizontalDivider);
-        System.out.print(firstAlpha);
-
         int i = 0;
         while(i < board.getTotalPositions()) {
-            System.out.print(singleSpace);
-            System.out.print(positions.get(i));
-            System.out.print(singleSpace + verticalDivider);
-            if(i % board.getRowSize() == (board.getRowSize() - 1)){
-                System.out.print(newLine);
-                System.out.println(horizontalDivider);
-                if(i != board.getTotalPositions() - 1){
-                    System.out.print(singleSpace);
-                    System.out.print(alpha[(i/board.getRowSize()) + 1]);
-                    System.out.print(verticalDivider);
-                }
-            }
+            this.printCellLeft();
+            this.printCellContent(positions, i);
+            this.printCellRight(i);
             i++;
         }
     }
 
     public void displayShips(Board board){
+        this.printHeader();
+
         ArrayList<String> positions = board.getPositions();
-
-        System.out.print(colHeaders);
-        System.out.print(horizontalDivider);
-        System.out.print(firstAlpha);
-
         int i = 0;
         while(i < board.getTotalPositions()) {
-            System.out.print(singleSpace);
-            if(board.getFilledPositions().contains(i)){
-                int index = board.getFilledPositions().indexOf(i);
-                System.out.print(board.getShipMarkers().get(index));
-            } else {
-                System.out.print(positions.get(i));
-            }
-            System.out.print(singleSpace + verticalDivider);
-            if(i % board.getRowSize() == board.getRowSize() - 1){
-                System.out.print(newLine);
-                System.out.print(horizontalDivider);
-                if(i != board.getTotalPositions() - 1){
-                    System.out.print(singleSpace);
-                    System.out.print(alpha[(i/board.getRowSize()) + 1]);
-                    System.out.print(verticalDivider);
-                }
-            }
+            this.printCellLeft();
+            this.printShipContent(positions, i);
+            this.printCellRight(i);
             i++;
+        }
+    }
+
+    private void printHeader(){
+        System.out.print(colHeaders);
+        System.out.println(horizontalDivider);
+        System.out.print(firstAlpha);
+    }
+
+    private void printCellLeft(){
+        System.out.print(singleSpace);
+    }
+
+    private void printCellRight(int i){
+        System.out.print(singleSpace + verticalDivider);
+        if(i % board.getRowSize() == (board.getRowSize() - 1)){
+            System.out.println(horizontalDivider);
+            if(i != board.getTotalPositions() - 1){
+                System.out.print(singleSpace);
+                System.out.print(alpha[(i/board.getRowSize()) + 1]);
+                System.out.print(verticalDivider);
+            }
+        }
+    }
+
+    private void printCellContent(ArrayList<String> positions, int i){
+        System.out.print(positions.get(i));
+    }
+
+    private void printShipContent(ArrayList<String> positions, int i){
+        if(board.getFilledPositions().contains(i)){
+            int index = board.getFilledPositions().indexOf(i);
+            System.out.print(board.getShipMarkers().get(index));
+        } else {
+            System.out.print(positions.get(i));
         }
     }
 }
